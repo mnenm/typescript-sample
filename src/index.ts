f713eb24b8643b598078d23ab502f566837316e5
mnenm <naamjp@gmail.com>
2019-10-28 02:20:58 +0900

    using user

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+import { User } from "./domain/entities/user";
+
 export const greet = (name: string): string => `Hi, ${name}`;
 console.log(greet("test"));
+
+const user = new User(1, "test", "user", 1, "test@test.test", 42);
+console.log(user.toJson());
